14c80086c62e7788b88c9eddd56ef0e5f8e7a389
Lewis <lewis@pool-ipv6-pd.agg3.rtd.rsl-rtd.eircom.net>
2017-10-07 16:43:18 +0100

    Initial Commit

---
+++ Treatment Dental Benefit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Treatment Dental Benefit
+//
+//  Created by Lewis on 07/10/2017.
+//  Copyright © 2017 Lewis. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Treatment Dental Benefit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Treatment Dental Benefit
+//
+//  Created by Lewis on 07/10/2017.
+//  Copyright © 2017 Lewis. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
